c9ccc2485c793f89e23f3a0ebb1240e96b1d5b51
Keane Quibilan <arv.r2vq@gmail.com>
2021-11-30 01:22:34 -0500

    Move more generation into Javascript and organize script

--- js/index.js
+++ js/index.js
@@ -1,60 +1,99 @@
+let config = {
+  categoryCount: 6,
+  clueCount: 5,
+  animationTime: 75,
+  version: "v0.5.1"
+};
+
 let board = document.getElementById("board");
 let clue = document.getElementById("clue");
 let createForm = document.getElementById("createForm");
-let createGameButton = document.getElementById("createGame");
 let dailyDouble = document.getElementById("dailyDouble");
 let question = document.getElementById("question");
 let splash = document.getElementById("splash");
-let form = document.getElementById("create");
-let categoryCount = 6;
-let clueCount = 5;
-let animationTime = 75;
 
-// Event Listeners
-function onAnswerClick(answer, answerItem) {
-  let showClue = event => {
-    answer.removeEventListener("click", showClue);
-    answer.addEventListener("click", hideClue);
+/****************************
+ * General Helper functions *
+ ****************************/
 
-    clue.innerHTML = answerItem.answer;
-    question.innerHTML = answerItem.question;
-
-    if (answerItem.isDailyDouble) {
-      let sound = new Audio("snd/daily-double.mp3");
-      sound.volume = 0.25;
-      sound.play();
-      transition(board, dailyDouble)();
-      addClass(dailyDouble, "rotate");
-    } else {
-      transition(board, clue)();
-      addClass(clue, "grow");
-    }
-    addClass(answer, "answered");
-    removeClass(answer, "answer");
-    removeClass(answer, "dollar");
-  };
-  let hideClue = event => {
-    answer.removeEventListener("click", hideClue);
-    answer.addEventListener("click", showClue);
-    removeClass(answer, "answered");
-    addClass(answer, "answer");
-    addClass(answer, "dollar");
+Array.prototype.shuffle = function() {
+  let output = [...this];
+  for (let i = this.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = output[i];
+    output[i] = output[j];
+    output[j] = temp;
   }
-  return showClue;
+  return output;
+};
+
+HTMLElement.prototype.addChild = function(name) {
+  let element = document.createElement(name);
+  this.appendChild(element);
+  return element;
+};
+
+HTMLElement.prototype.addInputChild = function(inputId, inputType, inputClass, labelText, labelClass) {
+  this.addChild("label")
+    .setProperty("for", inputId)
+    .addClass(labelClass)
+    .setText(labelText);
+
+  let input = this.addChild("input")
+    .setProperty("type", inputType)
+    .setId(inputId);
+  if (inputClass) {
+    input.addClass(inputClass);
+  }
+
+  return input;
 }
 
-function onRemoveAnimation(element, animation) {
-  return event => removeClass(element, animation);
-}
+HTMLElement.prototype.addClass = function(className) {
+  this.classList.add(className);
+  return this;
+};
 
-function transition(from, to) {
-  return () => {
-    hide(from);
-    show(to);
-  };
-}
+HTMLElement.prototype.removeClass = function(className) {
+  this.classList.remove(className);
+  return this;
+};
 
-function onFileUploaded(e) {
+HTMLDocument.prototype.on = function(eventName, callback) {
+  this.addEventListener(eventName, callback);
+  return this;
+};
+
+HTMLElement.prototype.on = function(eventName, callback) {
+  this.addEventListener(eventName, callback);
+  return this;
+};
+
+HTMLElement.prototype.off = function(eventName, callback) {
+  this.removeEventListener(eventName, callback);
+  return this;
+};
+
+HTMLElement.prototype.setId = function(id) {
+  this.id = id;
+  return this;
+};
+
+HTMLElement.prototype.setProperty = function(name, value) {
+  this.setAttribute(name, value);
+  return this;
+};
+
+HTMLElement.prototype.setText = function(text) {
+  this.innerText = text;
+  return this;
+};
+
+/******************************
+ * General Callback Functions *
+ ******************************/
+
+splash.on("change", (e) => {
   let file = e.target.files[0];
   if (file && file.name) {
     file.text().then(result => {
@@ -62,22 +101,246 @@ function onFileUploaded(e) {
       playGame(data);
     })
   }
+});
+
+document.on("click", e => {
+  let element = e.target;
+  switch (element.id) {
+    case "clue":
+      transition(clue, question);
+      break;
+    case "createGame":
+      createFormQuestions();
+      break;
+    case "dailyDouble":
+      transition(dailyDouble, clue);
+      break;
+    case "download":
+      e.preventDefault();
+      download();
+      break;
+    case "question":
+      transition(question, board);
+      break;
+    case "doubleJeopardyButton":
+      e.preventDefault();
+      populateScores(400);
+      break;
+    case "jeopardyButton":
+      e.preventDefault();
+      populateScores(200);
+      break;
+    case "oldJeopardyButton":
+      e.preventDefault();
+      populateScores(100);
+      break;
+  }
+});
+
+/*******************
+ * General Actions *
+ *******************/
+
+function createSplash() {
+  splash.addChild("div")
+    .addClass("header")
+    .setText("Let's play Jeopardy!");
+
+  splash.addChild("div")
+    .addClass("subheader")
+    .setText(config.version);
+
+  let splashForm = splash.addChild("form")
+    .setId("splashForm");
+  splashForm.addInputChild("uploadFile", "file", null, "Upload Existing Game", "uploadFile")
+    .setProperty("name", "uploadFile")
+    .setProperty("accept", ".jsonpardy");
+  splashForm.addInputChild("createGame", "button", null, "Create New Game", "createGame")
+    .setProperty("name", "createGame");
 }
 
-function onCreateGameButtonClick() {
-  createFormQuestions();
-  transition(splash, createForm)();
+function transition(from, to) {
+  from.addClass("hidden");
+  to.removeClass("hidden");
 }
 
-function onDownloadClick(e) {
-  e.preventDefault();
+/*************************
+ * Game Helper Functions *
+ *************************/
+
+HTMLElement.prototype.setOnAnswerClick = function(answerItem) {
+  // Hide the dollar, show the clue screen, swap callback to hideClue
+  let showClue = event => {
+    this.off("click", showClue)
+      .on("click", hideClue);
+    clue.setText(answerItem.answer);
+    question.setText(answerItem.question);
+    if (answerItem.isDailyDouble) {
+      playAudio("snd/daily-double.mp3", 0.25);
+      transition(board, dailyDouble);
+      dailyDouble.addClass("rotate");
+    } else {
+      transition(board, clue);
+      clue.addClass("grow");
+    }
+    this.addClass("answered")
+      .removeClass("answer")
+      .removeClass("dollar");
+  };
+
+  // Show the dollar, swap callback to showClue
+  let hideClue = event => {
+    this.off("click", hideClue)
+      .on("click", showClue)
+      .removeClass("answered")
+      .addClass("answer")
+      .addClass("dollar");
+  }
+
+  return this.on("click", showClue);
+};
+
+HTMLElement.prototype.setOnRemoveAnimation = function(animation) {
+  let onRemoveAnimation = () => {
+    this.removeClass(animation);
+  };
+  return this.on("animationend", onRemoveAnimation)
+    .on("webkitAnimationEnd", onRemoveAnimation);
+};
+
+/****************
+ * Game Actions *
+ ****************/
+
+function playGame(data) {
+  clue.setOnRemoveAnimation("grow");
+
+  dailyDouble.setText("DAILY DOUBLE");
+  dailyDouble.setOnRemoveAnimation("rotate");
+
+  let categories = [];
+  let clues = [];
+  data.forEach((categoryItem) => {
+    let column = board.addChild("div")
+      .addClass("column");
+
+    let category = column.addChild("div")
+      .addClass("tile")
+      .addClass("answered")
+      .setText(categoryItem.name);
+    categories.push(category);
+
+    categoryItem.answers.forEach((answerItem) => {
+      let answer = column.addChild("div")
+        .addClass("tile")
+        .addClass("answered")
+        .setText(answerItem.value)
+        .setOnAnswerClick(answerItem);
+      clues.push(answer);
+    });
+
+  });
+
+  clues.flatMap(e => e)
+    .shuffle()
+    .forEach((tile, i) => {
+      setTimeout(() => {
+        tile.removeClass("answered")
+          .addClass("answer")
+          .addClass("dollar");
+      }, config.animationTime * i);
+    });
+
+  playAudio("snd/board-fill.mp3");
+  categories.forEach((tile, i) => {
+    setTimeout(() => {
+      tile.removeClass("answered")
+        .addClass("category");
+    }, config.animationTime * clues.length + config.animationTime * 10 * (i + 1));
+  });
+
+  transition(splash, board);
+}
+
+function playAudio(url, volume) {
+  let sound = new Audio(url);
+  if (volume) {
+    sound.volume = volume;
+  }
+  sound.play();
+}
+
+/****************
+ * Form Actions *
+ ****************/
+
+function createFormQuestions() {
+  createForm.addChild("h1")
+    .setText("Create A New Game From Scratch");
+
+  let form = createForm.addChild("form")
+    .setId("create")
+    .addClass("createForm");
+
+  form.addChild("button")
+    .addClass("scoreButton")
+    .setId("oldJeopardyButton")
+    .setText("Populate Old Jeopardy Scores");
+
+  form.addChild("button")
+    .addClass("scoreButton")
+    .setId("jeopardyButton")
+    .setText("Populate Regular Jeopardy Scores");
+
+  form.addChild("button")
+    .addClass("scoreButton")
+    .setId("doubleJeopardyButton")
+    .setText("Populate DOUBLE Jeopardy Scores");
+
+  let formQuestions = form.addChild("div")
+    .addClass("formQuestions");
+
+  for (let i = 0; i < config.categoryCount; i++) {
+    let cat = "cat" + i;
+
+    let catDiv = formQuestions.addChild("div")
+      .addClass("formSection");
+
+    catDiv.addInputChild(cat, "text", null, "Category " + (i + 1), "categoryLabel");
+    for (let j = 0; j < config.clueCount; j++) {
+      catDiv.addInputChild(cat + "clue" + j, "text", "clueInput", "Clue " + (j + 1), "clueLabel");
+      catDiv.addInputChild(cat + "question" + j, "text", "questionInput", "Expected Response", "questionLabel");
+      catDiv.addInputChild(cat + "price" + j, "text", "priceInput", "Price", "priceLabel");
+      catDiv.addInputChild(cat + "daily" + j, "checkbox", "dailyInput", "Daily Double?", "dailyDoubleLabel");
+    }
+  }
+
+  form.addChild("div")
+    .addClass("gameNameSection")
+    .addInputChild("gameName", "text", "gameNameInput", "File Name", "gameNameLabel");
+
+  form.addInputChild("download", "submit", null, "Download Game", "download");
+
+  transition(splash, createForm);
+}
+
+function populateScores(base) {
+  for (let i = 0; i < config.categoryCount; i++) {
+    for (let j = 0; j < config.clueCount; j++) {
+      let price = (j + 1) * base;
+      document.getElementById("cat" + i + "price" + j).value = price;
+    }
+  }
+}
+
+function download() {
   let name = document.getElementById("gameName").value;
 
   let data = [];
-  for (let i = 0; i < categoryCount; i++) {
+  for (let i = 0; i < config.categoryCount; i++) {
     let cat = "cat" + i;
     let answers = [];
-    for (let j = 0; j < clueCount; j++) {
+    for (let j = 0; j < config.clueCount; j++) {
       answers.push({
         answer: document.getElementById(cat + "clue" + j).value,
         question: document.getElementById(cat + "question" + j).value,
@@ -94,223 +357,21 @@ function onDownloadClick(e) {
   }
 
   if (name) {
-    download(JSON.stringify(data), name + ".jsonpardy", "text/plain");
+    let a = document.createElement("a");
+    let file = new Blob([JSON.stringify(data)], {
+      type: "text/plain"
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = name + ".jsonpardy";
+    a.click();
   } else {
     alert("Please enter a name!");
   }
 }
 
-// Element Helpers
-function hide(element) {
-  addClass(element, "hidden");
-}
-
-function show(element) {
-  removeClass(element, "hidden");
-}
-
-function addAttribute(element, attribute) {
-  element.setAttribute(attribute.name, attribute.value);
-}
-
-function addClass(element, className) {
-  element.classList.add(className);
-}
-
-function removeClass(element, className) {
-  element.classList.remove(className);
-}
-
-// Create Elements
-function createElement(name, attributes, classes) {
-  let element = document.createElement(name);
-  if (attributes) {
-    attributes.forEach(attribute => {
-      addAttribute(element, attribute);
-    });
-  }
-  if (classes) {
-    classes.forEach(className => {
-      addClass(element, className)
-    });
-  }
-  return element;
-}
-
-function createInput(inputId, inputType, inputClass, labelText, labelClass, parent) {
-  let inputLabel = createElement(
-    "label",
-    [attr("for", inputId)],
-    [labelClass]
-  );
-  inputLabel.innerHTML = labelText;
-  parent.appendChild(inputLabel);
-  let input = createElement(
-    "input",
-    [attr("type", inputType)],
-    inputClass ? [inputClass] : []
-  );
-  input.id = inputId;
-  parent.appendChild(input);
-}
-
-function attr(name, value) {
-  return {
-    name: name,
-    value: value
-  };
-}
-
-function createFormQuestions() {
-  let oldJeopardyButton = createElement("button", [], ["scoreButton"]);
-  oldJeopardyButton.id = "oldJeopardyButton";
-  oldJeopardyButton.innerHTML = "Populate Old Jeopardy Scores";
-  form.appendChild(oldJeopardyButton);
-
-  let jeopardyButton = createElement("button", [], ["scoreButton"]);
-  jeopardyButton.id = "jeopardyButton";
-  jeopardyButton.innerHTML = "Populate Regular Jeopardy Scores";
-  form.appendChild(jeopardyButton);
-
-  let doubleJeopardyButton = createElement("button", [], ["scoreButton"]);
-  doubleJeopardyButton.id = "doubleJeopardyButton";
-  doubleJeopardyButton.innerHTML = "Populate DOUBLE Jeopardy Scores"
-  form.appendChild(doubleJeopardyButton);
-
-  let formQuestions = createElement("div", [], ["formQuestions"]);
-  form.appendChild(formQuestions);
-
-  for (let i = 0; i < categoryCount; i++) {
-    let cat = "cat" + i;
-
-    let catDiv = createElement("div", [], ["formSection"]);
-    formQuestions.appendChild(catDiv);
-
-    createInput(cat, "text", null, "Category " + (i + 1), "categoryLabel", catDiv);
-    for (let j = 0; j < clueCount; j++) {
-      createInput(cat + "clue" + j, "text", "clueInput", "Clue " + (j + 1), "clueLabel", catDiv);
-      createInput(cat + "question" + j, "text", "questionInput", "Expected Response", "questionLabel", catDiv);
-      createInput(cat + "price" + j, "text", "priceInput", "Price", "priceLabel", catDiv);
-      createInput(cat + "daily" + j, "checkbox", "dailyInput", "Daily Double?", "dailyDoubleLabel", catDiv);
-    }
-  }
-
-  let gameNameSection = createElement("div", [], ["gameNameSection"]);
-  form.appendChild(gameNameSection);
-  createInput("gameName", "text", "gameNameInput", "File Name", "gameNameLabel", gameNameSection);
-
-  createInput("download", "submit", null, "Download Game", "download", form);
-}
-
-// User Actions
-function download(content, fileName, contentType) {
-  let a = createElement("a");
-  let file = new Blob([content], {
-    type: contentType
-  });
-  a.href = URL.createObjectURL(file);
-  a.download = fileName;
-  a.click();
-}
-
-function playGame(data) {
-  let categories = [];
-  let clues = [];
-  data.forEach((categoryItem) => {
-    let column = createElement("div", [], ["column"]);
-
-    let category = createElement("div", [], ["tile", "answered"]);
-    category.innerHTML = categoryItem.name;
-    column.appendChild(category);
-    categories.push(category);
-
-    categoryItem.answers.forEach((answerItem) => {
-      let answer = createElement("div", [], ["tile", "answered"]);
-      answer.innerHTML = answerItem.value;
-
-      answer.addEventListener("click", onAnswerClick(answer, answerItem));
-      column.appendChild(answer);
-      clues.push(answer);
-    });
-
-    board.appendChild(column);
-  });
-
-  shuffle(clues.flatMap(e => e))
-    .forEach((tile, i) => {
-      setTimeout(() => {
-        removeClass(tile, "answered");
-        addClass(tile, "answer");
-        addClass(tile, "dollar");
-      }, animationTime * i);
-    });
-
-  new Audio("snd/board-fill.mp3").play();
-  categories.forEach((tile, i) => {
-    setTimeout(() => {
-      removeClass(tile, "answered");
-      addClass(tile, "category");
-    }, animationTime * clues.length + animationTime * 10 * (i + 1));
-  });
-
-  transition(splash, board)();
-}
-
-function shuffle(array) {
-  let output = [...array];
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = output[i];
-    output[i] = output[j];
-    output[j] = temp;
-  }
-  return output;
-}
-
-function populateScores(base) {
-  for (let i = 0; i < categoryCount; i++) {
-    for (let j = 0; j < clueCount; j++) {
-      let price = (j + 1) * base;
-      document.getElementById("cat" + i + "price" + j).value = price;
-    }
-  }
-}
-
-// Event Listeners
-clue.addEventListener("click", transition(clue, question));
-clue.addEventListener("webkitTransitionEnd", onRemoveAnimation(clue, "grow"));
-clue.addEventListener("transitionEnd", onRemoveAnimation(clue, "grow"));
-
-question.addEventListener("click", transition(question, board));
-
-dailyDouble.addEventListener("webkitTransitionEnd", onRemoveAnimation(dailyDouble, "rotate"));
-dailyDouble.addEventListener("transitionEnd", onRemoveAnimation(dailyDouble, "rotate"));
-dailyDouble.addEventListener("click", transition(dailyDouble, clue));
-
-createGameButton.addEventListener("click", onCreateGameButtonClick);
-
-splash.addEventListener("change", onFileUploaded);
-
-document.addEventListener("click", e => {
-  let element = e.target;
-  switch (element.id) {
-    case "download":
-      onDownloadClick();
-      break;
-    case "oldJeopardyButton":
-      e.preventDefault();
-      populateScores(100);
-      break;
-    case "jeopardyButton":
-      e.preventDefault();
-      populateScores(200);
-      break;
-    case "doubleJeopardyButton":
-      e.preventDefault();
-      populateScores(400);
-      break;
-  }
-  if (element.id == "download") {
-    onDownloadClick();
-  }
-});
+/***************
+ * Main Method *
+ ***************/
+(() => {
+  createSplash();
+})();
